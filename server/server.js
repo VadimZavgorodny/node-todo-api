5701749d375e50f1e8a18aeadda86e070ec9b5e5
VadimZavgorodny <va.zavgorodny@gmail.com>
2018-03-10 22:24:41 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
+var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -29,6 +30,27 @@ app.get('/todos', (req, res) => {
         });
     }, (err) => {
         res.status(400).send(err);
+    });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+    var todosID = req.params.id;
+
+    if (!ObjectID.isValid(todosID)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(todosID).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({
+            todo
+        })
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
